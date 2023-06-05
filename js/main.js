6e336dd91c5c6da2bdc7b30295d8587bc18645fa
DimaSurdiy <dsurdiy@ukr.net>
2023-06-05 06:54:16 +0300

    add smooth scrolling js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
+  $('.menu a, .to-top, .logo, .header__icon, .footer__logo').on('click', function (e) {
+		e.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		
+		$('body, html').animate({ scrollTop: top }, 1500);
+	});
+
+
   $('.blog').slick({
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow left"></button>',
@@ -15,7 +24,7 @@ $(function() {
   });
 
 
-  $('.menu__btn').on('click', function() {
+  $('.menu__btn, .menu a').on('click', function() {
     $('.header__top-inner').toggleClass('header__top-inner--active');
   });
 
