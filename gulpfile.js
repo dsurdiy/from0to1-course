c8052df45218c3bc757a40d3033a1f2dbb307f3c
DimaSurdiy <dsurdiy@ukr.net>
2023-06-07 13:52:14 +0300

    update styles gulp function

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-const { src, dest } = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-
-
+const { src, dest, watch } = require("gulp");
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
 
 function styles() {
-  return src('app/scss/style.scss')
-    .pipe(scss())
-    .pipe(dest('app/css'))
+  return src("app/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 versions"],
+        grid: true,
+      })
+    )
+    .pipe(dest("app/css"));
 }
 
+function watching() {
+  watch(["app/scss/**/*.scss"], styles);
+}
 
 exports.styles = styles;
+exports.watching = watching;
