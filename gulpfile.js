e18cf6addc169b309527ebe3cb3e758d82822230
DimaSurdiy <dsurdiy@ukr.net>
2023-06-07 07:07:13 +0300

    add styles gulp function

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest } = require('gulp');
+const scss = require('gulp-sass')(require('sass'));
+
+
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(scss())
+    .pipe(dest('app/css'))
+}
+
+
+exports.styles = styles;
