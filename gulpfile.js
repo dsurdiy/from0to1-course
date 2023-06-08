f9494efe26ad84f7f04fbd957f6177a2c45609a2
DimaSurdiy <dsurdiy@ukr.net>
2023-06-08 07:09:40 +0300

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
 const scss = require("gulp-sass")(require("sass"));
 const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
+
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
+}
 
 function styles() {
   return src("app/scss/style.scss")
@@ -13,12 +24,30 @@ function styles() {
         grid: true,
       })
     )
-    .pipe(dest("app/css"));
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "app/js/main.js",
+  ])
+  .pipe(concat("main.min.js"))
+  .pipe(uglify())
+  .pipe(dest("app/js"))
+  .pipe(browserSync.stream());
 }
 
 function watching() {
   watch(["app/scss/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
 }
 
 exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
 exports.watching = watching;
+
+exports.default = parallel(styles, scripts, browsersync, watching);
